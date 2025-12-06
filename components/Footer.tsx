b20a283c360af46b133e1252cf54c54c3f03e584
ADMIN <ngetichjustine1@gmail.com>
2025-12-06 13:51:47 +0300

    version001

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -78,7 +78,7 @@ export default function Footer() {
               <span className="text-base font-medium">@nescopharmafricaltd</span>
             </a>
 
-            <a href="https://www.facebook.com/nescopharmafricaltd" target="_blank" rel="noopener noreferrer"
+            <a href="https://www.facebook.com/profile.php?id=61577688909376&mibextid=rS40aB7S9Ucbxw6v" target="_blank" rel="noopener noreferrer"
               className="flex items-center space-x-3 hover:scale-105 transition-transform duration-300">
               <svg width="38" height="38" viewBox="0 0 24 24" fill="#1877F2" dangerouslySetInnerHTML={{ __html: siFacebook.svg }} />
               <span className="text-base font-medium">nescopharmafricaltd</span>
